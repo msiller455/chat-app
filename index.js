da1b73884e2a1516c3e7d1113586471537997c69
Michael <msiller455@gmail.com>
2019-08-07 16:41:05 -0700

    adds messages for users entering and leaving chat, and also population of chat room

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ app.get('/', function(req, res) {
     res.sendFile('/Users/michaelsiller/Code/practice/express/chat-app/index.html')
 })
 
+
+let population = 0
 io.on('connection', function(socket){
+    population += 1
+    socket.broadcast.emit('chat message', `New user has entered the chat, currently ${population} users`)
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg)
+    })
+    socket.broadcast.on('disconnect', function() {
+        population -= 1
+        io.emit('chat message', `User has left the chat, currently ${population}`)
     })
 })
 
