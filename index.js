59e99c66d36de4752fae325eb442f8b08f9fc772
Michael <msiller455@gmail.com>
2019-08-07 15:23:16 -0700

    refactors inline css to seperate file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let app = require('express')()
 let http = require('http').createServer(app)
 let io = require('socket.io')(http)
 
+app.use(require('express').static('public'))
+
 app.get('/', function(req, res) {
     res.sendFile('/Users/michaelsiller/Code/practice/express/chat-app/index.html')
 })
